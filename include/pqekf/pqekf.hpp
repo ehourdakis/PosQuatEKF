2e4441ede72b565b950cb75972d60110a306b374
ehourdakis <hourdakis.emmanouil@gmail.com>
2023-12-29 09:01:55 +0200

    added accelerometer and gyroscope measurements for ekf update

--- include/pqekf/pqekf.hpp
+++ include/pqekf/pqekf.hpp
@@ -1,4 +1,4 @@
-#include "ExtendedKalmanFilter.hpp"
+#include <ExtendedKalmanFilter.hpp>
 
 namespace ekf
 {
@@ -27,6 +27,7 @@ public:
      */
     PoseQuaternionEKF(const Eigen::Vector3d& position = Eigen::Vector3d::Zero(),
                       const Eigen::Quaterniond& orientation = Eigen::Quaterniond::Identity(),
+                      const Eigen::Vector3d& gyroscope_bias = Eigen::Vector3d::Zero(),
                       const double state_covariance = 1e-5,
                       const double process_covariance = 1e-5,
                       const double measurement_covariance = 1e+2,
@@ -36,7 +37,7 @@ public:
       _reject_outliers(reject_outliers),
       _ekf(new EKF())
     {
-        init_state(position, orientation);
+        init_state(position, orientation, gyroscope_bias);
 
         _ekf->setStateCovariance(EKF::Square::Identity()*state_covariance);
         _ekf->setProcessCovariance(EKF::Square::Identity()*process_covariance);
@@ -49,6 +50,15 @@ public:
     ~PoseQuaternionEKF()
     {
 
+    }
+
+    Eigen::Quaterniond ensureQuaternionContinuity(const Eigen::Quaterniond& new_orientation) {
+        Eigen::Quaterniond adjusted_orientation = new_orientation;
+        if (adjusted_orientation.dot(_prev_orientation) < -0.1) {
+            adjusted_orientation.coeffs() = -adjusted_orientation.coeffs();
+        }
+        _prev_orientation = adjusted_orientation;
+        return adjusted_orientation;
     }
 
     /**
@@ -77,30 +87,98 @@ public:
      * @param position The position component of the measurement.
      * @param orientation The orientation component represented as a normalized quaternion.
      */
-    const State update(const Eigen::Vector3d& position, const Eigen::Quaterniond& orientation)
+    const State update(const Eigen::Vector3d& position, const Eigen::Quaterniond& orientation, 
+        const Eigen::Vector3d& position_acceleration, const Eigen::Vector3d& angular_velocity)
     {
+        Eigen::Quaterniond adjusted_orientation = ensureQuaternionContinuity(orientation);
+
         Measurement measurement;
-        measurement(0) = position(0);
-        measurement(1) = position(1);
-        measurement(2) = position(2);
-        measurement(3) = orientation.w();
-        measurement(4) = orientation.x();
-        measurement(5) = orientation.y();
-        measurement(6) = orientation.z();
+        measurement(0)  = position(0);
+        measurement(1)  = position(1);
+        measurement(2)  = position(2);
+        measurement(3)  = adjusted_orientation.w();
+        measurement(4)  = adjusted_orientation.x();
+        measurement(5)  = adjusted_orientation.y();
+        measurement(6)  = adjusted_orientation.z();
+
+        measurement(7)  = position_acceleration(0);
+        measurement(8)  = position_acceleration(1);
+        measurement(9)  = position_acceleration(2);
+        measurement(10) = angular_velocity(0);
+        measurement(11) = angular_velocity(1);
+        measurement(12) = angular_velocity(2);
 
         auto x_ekf = _ekf->update(measurement, (_reject_outliers)?_outlier_threshold:-1.0); 
 
         return x_ekf;
     }
 
-    /**
-     * @brief Return the current EKF state.
-     */
-    inline const State& getState() 
-    { 
-        return _ekf->getState();
+    const State update_slam(const Eigen::Vector3d& position, const Eigen::Quaterniond& orientation)
+    {
+        Eigen::Quaterniond adjusted_orientation = ensureQuaternionContinuity(orientation);
+
+        Eigen::VectorXd measurement(7); // Create a vector of size 7
+
+        // Assign position
+        measurement(0) = position.x();
+        measurement(1) = position.y();
+        measurement(2) = position.z();
+
+        // Assign quaternion (w, x, y, z)
+        measurement(3) = adjusted_orientation.w();
+        measurement(4) = adjusted_orientation.x();
+        measurement(5) = adjusted_orientation.y();
+        measurement(6) = adjusted_orientation.z();
+
+        _ekf->updateWithSLAM(measurement, (_reject_outliers)?_outlier_threshold:-1.0); 
+
+        return getState();
     }
 
+    const State update_accelerometer_gyroscope(const Eigen::Vector3d& acceleration, const Eigen::Vector3d& angular_velocity, double dt)
+    {
+        // Perform the gyroscope update
+        _ekf->updateWithAccelerometerGyroscope(acceleration, angular_velocity, (_reject_outliers) ? _outlier_threshold : -1.0);
+
+        // Return the updated state
+        return getState();
+    }
+
+    const State update_accelerometer(const Eigen::Vector3d& acceleration, double dt)
+    {
+        // Perform the accelerometer update
+        _ekf->updateWithAccelerometer(acceleration, (_reject_outliers) ? _outlier_threshold : -1.0);
+
+        // Return the updated state
+        return getState();
+    }
+
+    const State update_gyroscope(const Eigen::Vector3d& angular_velocity, double dt)
+    {
+        // Perform the gyroscope update
+        _ekf->updateWithGyroscope(angular_velocity, (_reject_outliers) ? _outlier_threshold : -1.0);
+
+        // Return the updated state
+        return getState();
+    }
+
+    const State getState() {
+        State state = _ekf->getState();
+
+        // Extract the quaternion from the state
+        Eigen::Quaterniond current_orientation(state(9), state(10), state(11), state(12));
+
+        // Ensure continuity with the previous orientation
+        current_orientation = ensureQuaternionContinuity(current_orientation);
+
+        // Update the state with the adjusted quaternion
+        state(9) = current_orientation.w();
+        state(10) = current_orientation.x();
+        state(11) = current_orientation.y();
+        state(12) = current_orientation.z();
+
+        return state;
+    }
     /**
      * @brief Initialize the EKF state using a position and quaternion.
      *
